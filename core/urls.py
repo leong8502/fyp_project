077a69e4c10136e6752c0ce2242debe94704f212
leong8502 <126236078+leong8502@users.noreply.github.com>
2025-12-30 20:03:48 +0800

    temp home page

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ from . import views  # for home view
 urlpatterns = [
     path('', views.home, name='home'),
     path('match/', match_jobs, name='match_jobs'),
-    path('home/', views.client_home, name='client_home'),
+    path('client_home/', views.client_home, name='client_home'),
     path('project/', views.client_project, name='client_project'),
     path('about-us/', views.client_about, name='client_about'),
     path('message/', views.client_chat, name='client_chat'),
