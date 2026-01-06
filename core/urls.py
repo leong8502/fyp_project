ad8c413ed825f380f6ae7e65777e62d6760ff05d
leong8502 <126236078+leong8502@users.noreply.github.com>
2026-01-06 15:33:08 +0800

    minor change

--- core/urls.py
+++ core/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     path('client_home/', views.client_home, name='client_home'),
     path('client_profile/', views.client_profile, name='client_profile'),
     path('client_editProfile/', views.client_editProfile, name='client_editProfile'),
-    path('project/', views.client_project, name='client_project'),
-    path('projectCreate/', views.client_projectCreate, name='client_projectCreate'),
-    path('projectInfo/', views.client_projectInfo, name='client_projectInfo'),
+    path('client_project/', views.client_project, name='client_project'),
+    path('client_projectCreate/', views.client_projectCreate, name='client_projectCreate'),
+    path('client_projectInfo/', views.client_projectInfo, name='client_projectInfo'),
     path('about-us/', views.client_about, name='client_about'),
-    path('message/', views.client_chat, name='client_chat'),
+    path('client_chat/', views.client_chat, name='client_chat'),
 ]
