a9177e64487f89c48286a4acc4a1b6c5fe5a0712
YXing717 <gunyx-wp22@student.tarc.edu.my>
2026-03-11 22:47:56 +0800

    fix

--- fyp_project/settings.py
+++ fyp_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv  # type: ignore[import-untyped]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
